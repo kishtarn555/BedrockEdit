d418c043c9ccdaa7d27a6d8efc7eb46c72cbc070
Hector Fernando Ricardez Lara <hec_montemoon@hotmail.com>
2023-11-01 08:39:56 -0600

    block variables

--- scripts/variables.ts
+++ scripts/variables.ts
@@ -1,7 +1,10 @@
-import { Vector3 } from "@minecraft/server"
+import { Vector3, BlockPermutation } from "@minecraft/server"
 
 let coordinates1:Vector3|undefined
 let coordinates2:Vector3|undefined
+
+let block1:BlockPermutation | undefined
+let block2:BlockPermutation | undefined
 
 class BetsCoords {
 
@@ -26,4 +29,33 @@ class BetsCoords {
 }
 
 
-export { BetsCoords }
+class BetsBlocks {
+
+    static setBlock1(block:BlockPermutation) {
+        block1 = block;
+    }
+
+    static setBlock2(block:BlockPermutation) {
+        block2 = block;
+    }
+    
+    static getBlock1() {
+        return block1
+    }
+
+    
+    static getBlock2() {
+        return block2
+    }
+
+    static isBlock1Valid() {
+        return block1 != null ;
+    }
+
+    static isBlock2Valid() {
+        return block2 != null;
+    }
+}
+
+
+export { BetsCoords, BetsBlocks }
