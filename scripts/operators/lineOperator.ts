f9685542bdd9be7c7872d4ac3630b0c3d00576d3
Hector Fernando Ricardez Lara <hec_montemoon@hotmail.com>
2023-11-02 13:37:48 -0600

    Better line message + bugfix

--- scripts/operators/lineOperator.ts
+++ scripts/operators/lineOperator.ts
@@ -29,7 +29,7 @@ export default class OperatorLine extends BtsOperator {
         let result: OperatorResult
         try {
             let pos1: Vector3 = BetsCoords.getBlock1()!
-            let pos2: Vector3 = BetsCoords.getBlock1()!
+            let pos2: Vector3 = BetsCoords.getBlock2()!
     
             let points = plot3D(
                 pos1.x,
@@ -40,7 +40,8 @@ export default class OperatorLine extends BtsOperator {
                 pos2.z,
             )
             let commit:Commit=new  Commit("Line operation");
-            
+            const length = points.length;
+            let blocksChanged = 0;
             //NOTE: This might need a bump after setBlocks is added
             for (const point of points) {
                 // world.getDimension(dimension).runCommand(`setblock ${point.x} ${point.y} ${point.z} ${blockStatement}`);
@@ -49,18 +50,12 @@ export default class OperatorLine extends BtsOperator {
                 let block = world.getDimension("overworld").getBlock(point)
                 if (block == null) continue;
                 commit.setBlockPermutation(world.getDimension("overworld"), point, BetsBlocks.getBlock1()!);
+                blocksChanged++;
             }
             History.AddCommit(commit);
             return {
-                status: "success",
-                message: [
-                    pos1.x,
-                    pos1.y,
-                    pos1.z,
-                    pos2.x,
-                    pos2.y,
-                    pos2.z
-                ].join(",")
+                status: length === blocksChanged?"success":"warning",
+                message: `Line placed, set ${blocksChanged}/${length} blocks`
             }
             
         } catch (error) {
