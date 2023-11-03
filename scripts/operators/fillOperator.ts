7f3c31f9de72908899a45a2e724dbcdbe72b51c6
Hector Fernando Ricardez Lara <hec_montemoon@hotmail.com>
2023-11-03 04:48:47 -0600

    Fill operation spreads along ticks

--- scripts/operators/fillOperator.ts
+++ scripts/operators/fillOperator.ts
@@ -1,11 +1,11 @@
-import { CommandError, Dimension, Vector3, world } from "@minecraft/server"
+import { CommandError, Dimension, Vector3, world,system } from "@minecraft/server"
 import { BetsBlockPlacer, BetsBlocks, BetsCoords } from "../variables"
 import OperatorResult from "./OperatorResult"
 import BtsOperator, { OperatorReturner } from "./Operator"
 import { Commit } from "../commits/commit"
 import History from "../commits/history"
 import Workspace from "../workspace"
-import { ChainReturner, TickChain } from "../tickScheduler/scheduler"
+import { ChainReturner, TickChain, TickForeach } from "../tickScheduler/scheduler"
 export default class OperatorFill extends BtsOperator {
 
     workspace: Workspace | undefined
@@ -16,7 +16,7 @@ export default class OperatorFill extends BtsOperator {
     run(): Promise<OperatorResult> {
         return new TickChain<undefined, OperatorResult>()
             .first(this.validate)
-            .finally(this.fill)
+            .finally(this.fill.bind(this))
             .run(undefined);
 
     }
@@ -24,7 +24,7 @@ export default class OperatorFill extends BtsOperator {
         this.workspace = new Workspace(BetsCoords.getBlock1()!, BetsCoords.getBlock2()!, this.dimension!, "fill_operator");
         this.workspace.load();
     }
-    private validate(returner: OperatorReturner) {
+    private async validate(returner: OperatorReturner) {
         if (!BetsCoords.isThereAValidWorkspace()) {
             returner.breakAndReturn({
                 status: "error",
@@ -41,7 +41,18 @@ export default class OperatorFill extends BtsOperator {
         }
     }
 
-    private fill(returner: OperatorReturner): OperatorResult {
+    private * coords(x1: number, y1: number, z1: number, x2: number, y2: number, z2: number) {
+        for (let x = x1; x <= x2; x++) {
+            for (let y = y1; y <= y2; y++) {
+                for (let z = z1; z <= z2; z++) {
+                    yield { x: x, y: y, z: z };
+                }
+            }
+        }
+
+    }
+
+    private async fill(returner: OperatorReturner): Promise<OperatorResult> {
         let pos1: Vector3 = BetsCoords.getBlock1()!
         let pos2: Vector3 = BetsCoords.getBlock2()!
 
@@ -54,22 +65,30 @@ export default class OperatorFill extends BtsOperator {
         let z1 = Math.min(pos1.z, pos2.z);
         let z2 = Math.max(pos1.z, pos2.z);
         let area = (x2 - x1 + 1) * (y2 - y1 + 1) * (z2 - z1 + 1);
-        let commit: Commit = new Commit(`Fill operation ${area} blocks`);
         let blocksChanged = 0;
-        for (let x = x1; x <= x2; x++)
-            for (let y = y1; y <= y2; y++)
-                for (let z = z1; z <= z2; z++) {
+        world.sendMessage("ReachHere");
+        try {
+            let fors = new TickForeach((pos: Vector3) => {
+                let commit: Commit = new Commit(`Fill operation blocks`);
 
-                    // world.getDimension(dimension).runCommand(`setblock ${point.x} ${point.y} ${point.z} ${blockStatement}`);
-                    //FIXME: Support multidimension
-                    //FIXME: Add arguments
-                    let point = { x: x, y: y, z: z };
-                    let block = world.getDimension("overworld").getBlock(point)
-                    if (block == null) continue;
-                    blocksChanged++;
-                    commit.setBlockPermutation(world.getDimension("overworld"), point, BetsBlocks.getBlock1()!);
-                }
-        History.AddCommit(commit);
+                // world.getDimension(dimension).runCommand(`setblock ${point.x} ${point.y} ${point.z} ${blockStatement}`);
+                //FIXME: Support multidimension
+                //FIXME: Add arguments
+                let point = { x: pos.x, y: pos.y, z: pos.z };
+                let block = world.getDimension("overworld").getBlock(point)
+                if (block == null) return;
+                blocksChanged++;
+                commit.setBlockPermutation(world.getDimension("overworld"), point, BetsBlocks.getBlock1()!);
+                History.AddCommit(commit);
+            },
+                500);
+            // @ts-ignore
+            await fors.runOnIterable(this.coords(x1, y1, z1, x2, y2, z2))
+        } catch (error) {
+            world.sendMessage((error as Error).stack ?? "lol")
+        }
+
+        world.sendMessage("But not here");
         return ({
             status: area === blocksChanged ? "success" : "warning",
             message: `Filled ${blocksChanged}/${area}`
--- scripts/tickScheduler/scheduler.ts
+++ scripts/tickScheduler/scheduler.ts
@@ -1,17 +1,17 @@
-import { system } from "@minecraft/server"
+import { system,world } from "@minecraft/server"
 
 
 export class ChainReturner<ChainOut> {
-    resolver: (value:ChainOut)=>void;
-    private resolved:boolean;
-    constructor(resolver: (value:ChainOut)=>void) {
-        this.resolver=resolver;
-        this.resolved=false;
+    resolver: (value: ChainOut) => void;
+    private resolved: boolean;
+    constructor(resolver: (value: ChainOut) => void) {
+        this.resolver = resolver;
+        this.resolved = false;
     }
 
-    breakAndReturn(value:ChainOut) {
+    breakAndReturn(value: ChainOut) {
         this.resolver(value);
-        this.resolved=true;
+        this.resolved = true;
     }
 
     hasReturned() {
@@ -21,19 +21,19 @@ export class ChainReturner<ChainOut> {
 }
 
 class TickChainEnd<In, ChainIn, ChainOut> {
-    callback: (returner:ChainReturner<ChainOut>, arg: In) => ChainOut
+    callback: (returner: ChainReturner<ChainOut>, arg: In) => Promise<ChainOut>
     chain: TickChain<ChainIn, ChainOut>
 
-    constructor(callback: (returner:ChainReturner<ChainOut>, arg: In) => ChainOut, chain: TickChain<ChainIn, ChainOut>) {
+    constructor(callback: (returner: ChainReturner<ChainOut>, arg: In) => Promise<ChainOut>, chain: TickChain<ChainIn, ChainOut>) {
         this.callback = callback;
         this.chain = chain;
     }
 
-    _run(resolve: (value: ChainOut) => void, reject: (reason: any) => void, arg: In, returner:ChainReturner<ChainOut>) {
-        if (returner.hasReturned())return;
+    _run(resolve: (value: ChainOut) => void, reject: (reason: any) => void, arg: In, returner: ChainReturner<ChainOut>) {
+        if (returner.hasReturned()) return;
         system.run(() => {
-            let response = this.callback(returner,arg);
-            resolve(response);
+            let response = this.callback(returner, arg)
+                .then(response => resolve(response));
         });
     }
 
@@ -41,42 +41,43 @@ class TickChainEnd<In, ChainIn, ChainOut> {
         return new Promise<ChainOut>(
             (resolve, reject) => {
                 let returner = new ChainReturner<ChainOut>(resolve);
-                this.chain._run(resolve, reject, input,returner);
+                this.chain._run(resolve, reject, input, returner);
             }
         );
     }
 }
 
 class TickChainLink<In, Out, ChainIn, ChainOut> {
-    callback: (returner:ChainReturner<ChainOut>,arg: In) => Out
+    callback: (returner: ChainReturner<ChainOut>, arg: In) => Promise<Out>
     chain: TickChain<ChainIn, ChainOut>
     nextLink: TickChainLink<Out, any, ChainIn, ChainOut> | TickChainEnd<Out, ChainIn, ChainOut> | undefined
-    constructor(callback: (returner:ChainReturner<ChainOut>,arg: In) => Out, chain: TickChain<ChainIn, ChainOut>) {
+    constructor(callback: (returner: ChainReturner<ChainOut>, arg: In) => Promise<Out>, chain: TickChain<ChainIn, ChainOut>) {
         this.callback = callback;
         this.chain = chain;
     }
 
-    next<Out2 = undefined>(callback: (returner:ChainReturner<ChainOut>,arg: Out) => Out2) {
+    next<Out2 = undefined>(callback: (returner: ChainReturner<ChainOut>, arg: Out) => Promise<Out2>) {
         this.nextLink = new TickChainLink(callback, this.chain);
         return this.nextLink;
     }
 
-    finally(callback: (returner:ChainReturner<ChainOut>,arg: Out) => ChainOut) {
+    finally(callback: (returner: ChainReturner<ChainOut>, arg: Out) => Promise<ChainOut>) {
         this.nextLink = new TickChainEnd(callback, this.chain);
         return this.nextLink;
 
     }
 
 
-    _run(resolve: (value: ChainOut) => void, reject: (reason: any) => void, arg: In, returner:ChainReturner<ChainOut>) {
-        if (returner.hasReturned())return;
+    _run(resolve: (value: ChainOut) => void, reject: (reason: any) => void, arg: In, returner: ChainReturner<ChainOut>) {
+        if (returner.hasReturned()) return;
         system.run(() => {
-            let response = this.callback(returner,arg);
-            if (this.nextLink) {
-                this.nextLink?._run(resolve, reject, response, returner);
-            } else {
-                reject("Chain is broken!");
-            }
+            this.callback(returner, arg).then((response) => {
+                if (this.nextLink) {
+                    this.nextLink?._run(resolve, reject, response, returner);
+                } else {
+                    reject("Chain is broken!");
+                }
+            });
         });
     }
 }
@@ -85,15 +86,15 @@ class TickChainLink<In, Out, ChainIn, ChainOut> {
 export class TickChain<ChainIn, ChainOut> {
 
     FirstLink: TickChainLink<ChainIn, any, ChainIn, ChainOut> | undefined
-    first<Out>(callback: (returner:ChainReturner<ChainOut>,arg: ChainIn) => Out): TickChainLink<ChainIn, Out, ChainIn, ChainOut> {
+    first<Out>(callback: (returner: ChainReturner<ChainOut>, arg: ChainIn) => Promise<Out>): TickChainLink<ChainIn, Out, ChainIn, ChainOut> {
         this.FirstLink = new TickChainLink<ChainIn, Out, ChainIn, ChainOut>(callback, this);
 
         return this.FirstLink
     }
 
-    _run(resolve: (value: ChainOut) => void, reject: (reason: any) => void, input: ChainIn,returner:ChainReturner<ChainOut>) {
-        if (returner.hasReturned())return;
-        this.FirstLink!._run(resolve, reject, input,returner);
+    _run(resolve: (value: ChainOut) => void, reject: (reason: any) => void, input: ChainIn, returner: ChainReturner<ChainOut>) {
+        if (returner.hasReturned()) return;
+        this.FirstLink!._run(resolve, reject, input, returner);
     }
 }
 
@@ -127,4 +128,34 @@ export class SchedulerPerTick {
     }
 
 
+}
+
+
+export class TickForeach<T> {
+    processor: (arg: T) => void
+    iterationsPerTick: number
+    constructor(processor: (arg: T) => void, iterationsPerTick: number) {
+        this.iterationsPerTick = iterationsPerTick;
+        this.processor = processor
+    }
+    async runOnIterable(iterable: Iterable<T>) {
+        return new Promise<void>(
+            (resolve, reject) => this.runBlock(iterable[Symbol.iterator](), resolve)
+        );
+    }
+
+    private runBlock(iterator: Iterator<T, any, undefined>, resolve: (value: void) => void) {
+        let iter = 0;
+        let cursor = iterator.next();
+        while (!cursor.done) {
+            this.processor(cursor.value);
+            if (iter >= this.iterationsPerTick) {
+                system.run(() => this.runBlock(iterator, resolve)); //Run remaining items on next tick
+                return;
+            }
+            cursor = iterator.next();
+            iter++;
+        }
+        resolve();
+    }
 }
