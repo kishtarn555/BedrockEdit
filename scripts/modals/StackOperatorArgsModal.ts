e0d09d64da46ac379954518c14b9946725769c18
Hector Fernando Ricardez Lara <hec_montemoon@hotmail.com>
2023-11-12 02:58:03 -0600

    fix stack arguments title

--- scripts/modals/StackOperatorArgsModal.ts
+++ scripts/modals/StackOperatorArgsModal.ts
@@ -3,41 +3,40 @@ import { StackParameters } from "../operators/stackOperator";
 import { Direction, Player } from "@minecraft/server";
 
 export class StackOperatorArgsModal {
-    modal : ModalFormData
-    constructor () {
+    modal: ModalFormData
+    constructor() {
         this.modal = new ModalFormData();
         this.modal
             .title(
                 {
-                    translate:"bets.modal.operator_arguments.title", 
-                    with: [
-                        "bets.operator.stack.name"
-                    ]
+                    translate: "bets.modal.operator_arguments.title",
+                    with: { rawtext: [{ translate: "bets.operator.stack.name" }] }
+
                 })
             .dropdown(
                 {
-                    translate:"bets.operator.stack.direction"
+                    translate: "bets.operator.stack.direction"
                 },
                 [
-                    {translate:"bets.util.directions.up"},   
-                    {translate:"bets.util.directions.down"},   
-                    {translate:"bets.util.directions.north"},   
-                    {translate:"bets.util.directions.east"},   
-                    {translate:"bets.util.directions.south"},  
-                    {translate:"bets.util.directions.west"},   
+                    { translate: "bets.util.directions.up" },
+                    { translate: "bets.util.directions.down" },
+                    { translate: "bets.util.directions.north" },
+                    { translate: "bets.util.directions.east" },
+                    { translate: "bets.util.directions.south" },
+                    { translate: "bets.util.directions.west" },
                 ]
             )
             .dropdown(
                 {
-                    translate:"bets.operator.stack.mode.name"
+                    translate: "bets.operator.stack.mode.name"
                 },
                 [
-                    {translate:"bets.operator.stack.mode.masked"},
-                    {translate:"bets.operator.stack.mode.replace"}
+                    { translate: "bets.operator.stack.mode.masked" },
+                    { translate: "bets.operator.stack.mode.replace" }
                 ]
             )
             .slider(
-                { translate:"bets.operator.stack.copies_count" },
+                { translate: "bets.operator.stack.copies_count" },
                 1,
                 30,
                 1,
@@ -46,7 +45,7 @@ export class StackOperatorArgsModal {
     }
 
 
-    async show(player:Player):Promise<StackParameters> {
+    async show(player: Player): Promise<StackParameters> {
         const response = await this.modal.show(player);
         if (response.canceled) {
             throw "cancelled";
@@ -60,17 +59,17 @@ export class StackOperatorArgsModal {
             Direction.West
         ][response.formValues![0] as number];
         const mode = ["masked", "replace"]
-            [response.formValues![1] as number] as "masked"|"replace";
+        [response.formValues![1] as number] as "masked" | "replace";
         const copies = response.formValues![2] as number;
 
         return {
-            direction:direction,
-            mask:mode,
-            copies:copies,
+            direction: direction,
+            mask: mode,
+            copies: copies,
         }
 
 
-        
+
 
     }
 }
